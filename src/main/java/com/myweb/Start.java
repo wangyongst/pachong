2323c212b0a8635ff2fa314f0c4dd0daa217964e
wangyongst <wangyongst@gmail.com>
2018-06-22 10:13:06 +0800

    office

--- src/main/java/com/myweb/Start.java
+++ src/main/java/com/myweb/Start.java
@@ -8,7 +8,7 @@ import java.util.concurrent.Executors;
 public class Start {
 
     private static final int MAX_THREADS = 50;
-    public static final int SLEEP = 2000;
+    public static final int SLEEP = 5000;
     public static ExecutorService executorService = Executors.newFixedThreadPool(MAX_THREADS);
 
     public static void main(String[] args){
